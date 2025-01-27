e3b30af257d3cba840060c0b2e0025b09edc3cf1
yipengmaxzhang <maxzhang2728@gmail.com>
2025-01-27 08:13:52 -0500

    added hello world and input/output commands

--- Lab1.py
+++ Lab1.py
@@ -1,12 +1,13 @@
 # Function 1: Write a simple Hello World program
 # This function should print "Hello, World!" to the screen.
 def hello_world():
-    # TODO: Implement this function
-    pass  # Replace with your code
+    print("Hello, World!")
 
 # Function 2: Get input and output with different variable types
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
-    # TODO: Implement this function
-    pass  # Replace with your code
+    name = input("Hello! What is your name?: ")
+    age = int(input("What is your age: "))
+    height = float(input("How tall are you, in feet?: "))
+    print("Welcome, " + name + ", you are " + str(age) + " years old and you are " + str(height) + " feet tall!")
